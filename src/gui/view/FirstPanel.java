dbfa4315ce4402b11d47b01e3f98276fe749f75c
Michael <mjmjulien10@gmail.com>
2018-12-05 08:59:25 -0700

    chatbot

--- src/gui/view/FirstPanel.java
+++ src/gui/view/FirstPanel.java
@@ -2,7 +2,11 @@ package gui.view;
 
 
 import gui.controller.GUIController;
-
+import java.awt.Dimension;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.*;
+import chat.controller.ChatController;
 
 i
 public class FirstPanel extends JPanel
@@ -39,21 +43,35 @@ public class FirstPanel extends JPanel
 	
 	private void setupPanel()
 	{
-		
+		this.setLayout(appLayout);
+		this.setPreferredSize(new Dimension(800, 600));
+		this.setBackground(Color.magenta);
+		this.add(chatPane);
+		this.add(chatButton);
+		this.add(saveButton);
+		this.add(loadButton);
+		this.add(checkerButton);
+		this.add(chatField);
 	}
 	
 	private void setupLayout()
 	{
-		
+		//cords
 	}
 	
 	private void setupListeners()
 	{
-		
+		chatButton.addActionListener(new ActionListener);
 	}
 	
 	private void setupScrollPane()
 	{
+		chatArea.setEditable(false);
+		chatArea.setLineWrap(true);
+		chatArea.setWrapStyleWord(true);
 		
+		chatPane.setViewportView(chatArea);
+		chatPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		chatPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
 	}
 }
